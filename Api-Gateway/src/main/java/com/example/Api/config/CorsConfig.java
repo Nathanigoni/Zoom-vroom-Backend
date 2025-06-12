10e88fae9d0be1838476d6aefa1f7eb56881168c
Fahd <fahdadebayo02@gmail.com>
2025-06-12 18:10:20 +0100

    chore: update api gateway CORS cofiguration

--- Api-Gateway/src/main/java/com/example/Api/config/CorsConfig.java
+++ Api-Gateway/src/main/java/com/example/Api/config/CorsConfig.java
@@ -3,22 +3,22 @@ package com.example.Api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:3000"); // Add your frontend domain here
+        config.addAllowedOrigin("http://localhost:3000"); // Adjust as needed
         config.setAllowCredentials(true);
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+        return new CorsWebFilter(source);
     }
 }
